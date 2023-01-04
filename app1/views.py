d20f76f27886396cbbb8d97c8f013bba750d75f9
manish7840 <sameermanish86@gmail.com>
2023-01-04 22:59:44 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def radharani(request):
+    return render(request,'radharani.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def radharani1(request):
+    return render(request,'radharani1.html')
